037f7dfeabb1e8dafc0e5736b8a10255bd8eda5f
shambhavi0225 <97248801+shambhavi0225@users.noreply.github.com>
2024-08-20 20:08:41 +0530

    Add files via upload

---
+++ backend/config/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const connectionDb = mongoose.connection
+
+connectionDb.on('error',(error)=>{
+    console.log(error);
+})
+
+connectionDb.on('connected',()=>{
+    console.log("Connected to Database successfully.")
+})
+
+module.exports = connectionDb
